98cdeb8c1c7960b430a58a5a86c9eddf71f8f53b
pavancd <pavancd91@gmail.com>
2020-01-27 10:48:56 +0000

    change project structure

--- FastExpo/Main.java
+++ FastExpo/Main.java
@@ -1,11 +1,21 @@
 class Main {
     public static void main(String arg[]) {
-        long result = fastExpo(2, 13, 5);
+        long result = fastExpo(2, 8, 5);
         System.out.println(result);
     }
 
 
     private static long fastExpo(long a, long n, int MOD) {
+
+      /***
+      a =2, n=6, MOD=5
+      iteration
+      1 ans->1, a->4 n->3
+      2 ans->4, a->4 n->2
+      3 ans->4, a->1 n->1
+      ***/
+
+      
         long ans = 1;
 
         while(n>=1) {
--- Main.java
+++ Main.java
@@ -1,23 +1,21 @@
-class Main {
-    public static void main(String arg[]) {
-        long result = fastExpo(2, 13, 5);
-        System.out.println(result);
-    }
 
+  
+    
+    
+  
 
-    private static long fastExpo(long a, long n, int MOD) {
-        long ans = 1;
+  
+    
 
-        while(n>=1) {
-            if(n%2==0) {
-                a = (a*a)%MOD;
-                n /= 2;
-            } else {
-                ans = ans * a;
-                n -= 1;
-            }
-        }
+       
+           
+            
+        
+      
+        
+        
+      
+    
 
-        return ans;
-    }
-}
+    
+  
